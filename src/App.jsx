c557f0ed4917e6f3f0087172478f261a8e8268d9
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-27 11:12:53 +1000

    comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { io } from "socket.io-client";
 import { useEffect } from "react";
 
 export default function App() {
+  // useEffect creates connection on mount
   useEffect(() => {
     const socket = io("http://localhost:4000", {
       transports: ["websocket"],
     });
+    // emit message to server on mount
     socket.emit("message", "hello");
 
+    // handle "mesasge" from server and log msg
     socket.on("message", (msg) => {
       console.log("client got a message ", msg);
     });
 
+    // cleanup socket and disconnect
     return () => {
       socket.disconnect();
     };
