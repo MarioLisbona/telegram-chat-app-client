90a72a2bc06fd9a1ed62255533f21ad2bf968e22
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-29 13:09:20 +1000

    comments

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,11 @@ import ChatPage from "./oldComponents/ChatPage.jsx";
 const protocol =
   import.meta.env.MODE === "development" ? "http://" : "https://";
 
+// set server host and port based on environment
 let serverHost = import.meta.env.VITE_SERVER_HOST || "localhost";
 let serverPort = import.meta.env.VITE_SERVER_PORT || 4000;
 
+// socket connetion string
 const socket = io(`${protocol}${serverHost}:${serverPort}`, {
   transports: ["websocket"],
 });
