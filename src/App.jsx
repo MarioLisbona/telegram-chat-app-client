209621b4c74b3ab36842f5ef2695cbc9da4767d4
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-29 20:46:30 +1000

    dynamically constructs the WebSocket URL

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,29 @@ import Home from "./oldComponents/Home";
 import ChatPage from "./oldComponents/ChatPage.jsx";
 
 // Determine the protocol based on environment
-const protocol =
-  import.meta.env.MODE === "development" ? "http://" : "https://";
+// const protocol =
+//   import.meta.env.MODE === "development" ? "http://" : "https://";
 
-// set server host and port based on environment
-let serverHost = import.meta.env.VITE_SERVER_HOST || "localhost";
-let serverPort = import.meta.env.VITE_SERVER_PORT || 4000;
+// // set server host and port based on environment
+// let serverHost = import.meta.env.VITE_SERVER_HOST || "localhost";
+// let serverPort = import.meta.env.VITE_SERVER_PORT || 4000;
 
-console.log(`${protocol}${serverHost}:${serverPort}`);
+// console.log(`${protocol}${serverHost}:${serverPort}`);
 
-// socket connetion string
-const socket = io(`${protocol}${serverHost}:${serverPort}`, {
+// // socket connetion string
+// const socket = io(`${protocol}${serverHost}:${serverPort}`, {
+//   transports: ["websocket"],
+// });
+
+const protocol = window.location.protocol === "https:" ? "wss://" : "ws://";
+const host = window.location.hostname;
+const port = window.location.port;
+
+const socket = io(`${protocol}${host}:${port}`, {
   transports: ["websocket"],
 });
+
+console.log(`${protocol}${host}:${port}`);
 
 export default function App() {
   return (
