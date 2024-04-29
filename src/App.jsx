c3ee91f98cc5c5fa2feda247945ea679685d2f16
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-30 09:28:11 +1000

    hard coded webscoket url

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./oldComponents/Home";
 import ChatPage from "./oldComponents/ChatPage.jsx";
 
-// Determine the protocol based on environment
-// const protocol =
-//   import.meta.env.MODE === "development" ? "http://" : "https://";
-
-// // set server host and port based on environment
-// let serverHost = import.meta.env.VITE_SERVER_HOST || "localhost";
-// let serverPort = import.meta.env.VITE_SERVER_PORT || 4000;
-
-// console.log(`${protocol}${serverHost}:${serverPort}`);
-
-// // socket connetion string
-// const socket = io(`${protocol}${serverHost}:${serverPort}`, {
-//   transports: ["websocket"],
-// });
-
-const protocol = window.location.protocol === "https:" ? "wss://" : "ws://";
-const host = window.location.hostname;
-const port = window.location.port;
-
-const socket = io(`${protocol}${host}:${port}`, {
+const socket = io("http://localhost:4000", {
   transports: ["websocket"],
 });
-
-console.log(`${protocol}${host}:${port}`);
 
 export default function App() {
   return (
