96d2cd5689fa36af046af7d54f93dc390cf3a56c
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-30 18:42:32 +1000

    UseEffect setting message state

--- src/components/Chat/ChatPage.jsx
+++ src/components/Chat/ChatPage.jsx
@@ -1,12 +1,47 @@
+import { useEffect, useState, useRef } from "react";
 import ChatBody from "./body/ChatBody";
 import ChatUserPanel from "./user panel/ChatUserPanel";
 
-export default function ChatPage() {
+export default function ChatPage({ socket }) {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Function to handle incoming messages
+    const handleMessageResponse = (data) => {
+      // spread new message object into messages array
+      setMessages([...messages, data]);
+    };
+
+    // Function to handle incoming Telegram messages
+    const handleTelegramMessage = (data) => {
+      // create new message object with telegram message data
+      const telegramMessage = {
+        text: data.text,
+        name: `(telegram) ${data.from.first_name} ${data.from.last_name}`,
+        id: `${data.chat.id}-${data.chat.date}-${data.message_id}`,
+        socketID: "unknown",
+      };
+
+      // spread new telegram message object into messages array
+      setMessages([...messages, telegramMessage]);
+    };
+
+    // callbacks for different responses received on socket
+    socket.on("messageResponse", handleMessageResponse);
+    socket.on("telegramMessage", handleTelegramMessage);
+
+    // Clean up event listeners when component unmounts
+    return () => {
+      socket.off("messageResponse", handleMessageResponse);
+      socket.off("telegramMessage", handleTelegramMessage);
+    };
+  }, [socket, messages]);
+
   return (
     <div className="flex h-screen antialiased text-gray-800">
       <div className="flex flex-row h-full w-full overflow-x-hidden">
         <ChatUserPanel />
-        <ChatBody />
+        <ChatBody messages={messages} />
       </div>
     </div>
   );
