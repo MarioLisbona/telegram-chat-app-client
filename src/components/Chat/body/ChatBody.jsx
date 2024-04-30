6ecaea3dd5a1b4fe3fb9c953b9a7c987d15afe4c
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-30 18:42:57 +1000

    mapping messages array

--- src/components/Chat/body/ChatBody.jsx
+++ src/components/Chat/body/ChatBody.jsx
@@ -1,30 +1,20 @@
 import ChatFooter from "../footer/ChatFooter";
 import ChatMessageReceived from "./components/ChatMessageReceived";
 import ChatMessageSent from "./components/ChatMessageSent";
-export default function ChatBody() {
+export default function ChatBody({ messages }) {
   return (
     <div className="flex flex-col flex-auto h-full p-6">
       <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
         <div className="flex flex-col h-full overflow-x-auto mb-4">
           <div className="flex flex-col h-full">
             <div className="grid grid-cols-12 gap-y-2">
-              <ChatMessageSent />
-              <ChatMessageReceived />
-              <ChatMessageReceived />
-              <ChatMessageReceived />
-              <ChatMessageReceived />
-              <ChatMessageReceived />
-              <ChatMessageSent />
-              <ChatMessageReceived />
-              <ChatMessageSent />
-              <ChatMessageReceived />
-              <ChatMessageReceived />
-              <ChatMessageSent />
-              <ChatMessageReceived />
-              <ChatMessageReceived />
-              <ChatMessageReceived />
-              <ChatMessageSent />
-              <ChatMessageSent />
+              {messages.map((message, idx) =>
+                message.name === localStorage.getItem("userName") ? (
+                  <ChatMessageSent message={message} key={idx} />
+                ) : (
+                  <ChatMessageReceived message={message} key={idx} />
+                )
+              )}
             </div>
           </div>
         </div>
