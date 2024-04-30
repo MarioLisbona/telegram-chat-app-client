b5838df93e59ce919961bd6dc1cfe277f0473a73
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-30 22:36:39 +1000

    update styling for chat user

--- src/components/Chat/user panel/components/UserButton.jsx
+++ src/components/Chat/user panel/components/UserButton.jsx
@@ -1,13 +1,25 @@
 export default function UserButton({ user }) {
   const activeUser = user.userName;
   const userInitial = user.userName.charAt(0).toUpperCase();
-  console.log({ user });
+  // set username
+  const chatUser = localStorage.getItem("userName") == user.userName;
+  console.log(chatUser);
   return (
     <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-      <div className="flex items-center justify-center h-8 w-8 bg-orange-200 rounded-full">
+      <div
+        className={`flex items-center justify-center h-8 w-8 bg-orange-200 rounded-full ${
+          chatUser ? "border-2 border-indigo-500" : ""
+        }`}
+      >
         {userInitial}
       </div>
-      <div className="ml-2 text-sm font-semibold">{activeUser}</div>
+      <div
+        className={`ml-2 text-sm  ${
+          chatUser ? "text-indigo-500 font-bold" : "font-semibold"
+        }`}
+      >
+        {activeUser}
+      </div>
     </button>
   );
 }
