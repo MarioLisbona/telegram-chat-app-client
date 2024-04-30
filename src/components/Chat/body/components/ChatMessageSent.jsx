948345fb9677e192245f84032123f7a963b789cd
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-01 09:14:11 +1000

    username and styling for sent messages

--- src/components/Chat/body/components/ChatMessageSent.jsx
+++ src/components/Chat/body/components/ChatMessageSent.jsx
@@ -1,14 +1,19 @@
 export default function ChatMessageSent({ message }) {
   const text = message.text;
+  const userName = message.name;
   const userInitial = message.name.charAt(0).toUpperCase();
+
   return (
     <div className="col-start-6 col-end-13 p-3 rounded-lg">
-      <div className="flex items-center justify-start flex-row-reverse">
-        <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-          {userInitial}
-        </div>
-        <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-          <div>{text}</div>
+      <div className="flex flex-col items-end">
+        <div className="text-xs mb-1">{userName}</div>
+        <div className="flex items-center justify-start flex-row-reverse">
+          <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+            {userInitial}
+          </div>
+          <div className="relative mr-3 text-sm font-medium bg-indigo-100 py-2 px-4 shadow rounded-xl">
+            <div>{text}</div>
+          </div>
         </div>
       </div>
     </div>
