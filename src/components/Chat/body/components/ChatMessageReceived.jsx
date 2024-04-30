1aaf8b04fb042f91be925af300477c90d002cc83
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-30 20:22:01 +1000

    format message data and user initial

--- src/components/Chat/body/components/ChatMessageReceived.jsx
+++ src/components/Chat/body/components/ChatMessageReceived.jsx
@@ -1,12 +1,16 @@
 export default function ChatMessageReceived({ message }) {
+  const text = message.text;
+  const userInitial =
+    message.name.charAt(0) == "(" ? "TG" : message.name.charAt(0).toUpperCase();
+
   return (
     <div className="col-start-1 col-end-8 p-3 rounded-lg">
       <div className="flex flex-row items-center">
         <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-          A
+          {userInitial}
         </div>
         <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-          <div>{message.text}</div>
+          <div>{text}</div>
         </div>
       </div>
     </div>
--- src/components/Chat/body/components/ChatMessageSent.jsx
+++ src/components/Chat/body/components/ChatMessageSent.jsx
@@ -1,12 +1,14 @@
 export default function ChatMessageSent({ message }) {
+  const text = message.text;
+  const userInitial = message.name.charAt(0).toUpperCase();
   return (
     <div className="col-start-6 col-end-13 p-3 rounded-lg">
       <div className="flex items-center justify-start flex-row-reverse">
         <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-          A
+          {userInitial}
         </div>
         <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-          <div>{message.text}</div>
+          <div>{text}</div>
         </div>
       </div>
     </div>
