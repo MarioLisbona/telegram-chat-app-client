eaf0d3e0423478052295c948d59026716464260a
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-01 11:38:20 +1000

    use typingResponse useEffect

--- src/components/Chat/user panel/components/UserButton.jsx
+++ src/components/Chat/user panel/components/UserButton.jsx
@@ -1,25 +1,48 @@
-export default function UserButton({ user }) {
-  const activeUser = user.userName;
+import TypingStatus from "../../body/components/TypingStatus";
+import { useState, useEffect } from "react";
+
+export default function UserButton({ user, socket }) {
+  const [showTypingStatus, setShowTypingStatus] = useState(false);
+
+  useEffect(() => {
+    socket.on("typingResponse", (data) => {
+      if (data === user.userName) {
+        setShowTypingStatus(true);
+        const timeoutId = setTimeout(() => {
+          setShowTypingStatus(false);
+        }, 3000);
+        return () => clearTimeout(timeoutId);
+      }
+    });
+
+    return () => {
+      socket.off("typingResponse");
+    };
+  }, [socket, user.userName]);
+
   const userInitial = user.userName.charAt(0).toUpperCase();
   // set username
   const chatUser = localStorage.getItem("userName") == user.userName;
-  console.log(chatUser);
+
   return (
-    <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-      <div
-        className={`flex items-center justify-center h-8 w-8 bg-orange-200 rounded-full ${
-          chatUser ? "border-2 border-indigo-500" : ""
-        }`}
-      >
-        {userInitial}
-      </div>
-      <div
-        className={`ml-2 text-sm  ${
-          chatUser ? "text-indigo-500 font-bold" : "font-semibold"
-        }`}
-      >
-        {activeUser}
-      </div>
-    </button>
+    <div className="flex flex-col items-start justify-center">
+      <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
+        <div
+          className={`flex items-center justify-center h-8 w-8 bg-orange-200 rounded-full ${
+            chatUser ? "border-2 border-indigo-500" : ""
+          }`}
+        >
+          {userInitial}
+        </div>
+
+        <div
+          className={`ml-2 text-sm  ${
+            chatUser ? "text-indigo-500 font-bold" : "font-semibold"
+          }`}
+        >
+          {showTypingStatus ? <TypingStatus /> : user.userName}
+        </div>
+      </button>
+    </div>
   );
 }
