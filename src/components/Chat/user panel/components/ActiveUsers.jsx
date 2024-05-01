df6d59f511364cbcc8d5e9f86b13bb958b1c72e4
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-01 11:37:58 +1000

    add scoket, remove comments

--- src/components/Chat/user panel/components/ActiveUsers.jsx
+++ src/components/Chat/user panel/components/ActiveUsers.jsx
@@ -1,6 +1,6 @@
 import UserButton from "./UserButton";
 import { sortArrayByUsername } from "../../../../lib/chatUitls";
-export default function ActiveUser({ users }) {
+export default function ActiveUser({ users, socket }) {
   // assign current chat user and sort users array with current user at top
   const chatUser = localStorage.getItem("userName");
   const sortedUsers = sortArrayByUsername(users, chatUser);
@@ -13,17 +13,8 @@ export default function ActiveUser({ users }) {
         </span>
       </div>
       <div className="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
-        {/* <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-          <div className="flex items-center justify-center h-8 w-8 bg-gray-200 rounded-full">
-            M
-          </div>
-          <div className="ml-2 text-sm font-semibold">Marta Curtis</div>
-          <div className="flex items-center justify-center ml-auto text-xs text-white bg-red-500 h-4 w-4 rounded leading-none">
-            2
-          </div>
-        </button> */}
         {sortedUsers.map((user, idx) => (
-          <UserButton user={user} key={idx} />
+          <UserButton user={user} key={idx} socket={socket} />
         ))}
       </div>
       <div className="flex flex-row items-center justify-between text-xs mt-6">
