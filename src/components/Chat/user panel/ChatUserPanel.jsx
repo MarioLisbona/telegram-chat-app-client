12dbe3fc92f2b1291adf82babbb2c5678e9f15d1
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-01 15:13:34 +1000

    Update user panel width

--- src/components/Chat/user panel/ChatUserPanel.jsx
+++ src/components/Chat/user panel/ChatUserPanel.jsx
@@ -12,7 +12,7 @@ export default function ChatUserPanel({ socket, chatTitle }) {
     socket.on("newUserResponse", (data) => setUsers(data));
   }, [socket, users]);
   return (
-    <div className="flex flex-col py-8 pl-6 pr-2 w-64 bg-white flex-shrink-0">
+    <div className="flex flex-col py-8 pl-6 pr-2 w-80 bg-white flex-shrink-0">
       <ChatName chatTitle={chatTitle} />
       {/* <UserProfile /> */}
       <ActiveUsers users={users} socket={socket} />
