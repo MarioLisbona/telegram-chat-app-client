208bdc5e20f70a4dd742985886d920dc68890025
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-01 11:37:40 +1000

    add socket prop

--- src/components/Chat/user panel/ChatUserPanel.jsx
+++ src/components/Chat/user panel/ChatUserPanel.jsx
@@ -14,7 +14,7 @@ export default function ChatUserPanel({ socket }) {
     <div className="flex flex-col py-8 pl-6 pr-2 w-64 bg-white flex-shrink-0">
       <ChatName />
       {/* <UserProfile /> */}
-      <ActiveUsers users={users} />
+      <ActiveUsers users={users} socket={socket} />
     </div>
   );
 }
