9f0f3739d0afb23449fa903abe632411d967d79d
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-10 19:52:55 +1000

    Update login styling

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -81,26 +81,14 @@ export default function Login() {
               </div>
 
               <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  {/* <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                  />
-                  <label
-                    htmlFor="remember-me"
-                    className="ml-3 block text-sm leading-6 text-gray-900"
-                  >
-                    Remember me
-                  </label> */}
+                <div className="text-sm leading-6">
+                  <a href="#" className="font-semibold hover:text-slate-500">
+                    Sign Up
+                  </a>
                 </div>
 
                 <div className="text-sm leading-6">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
+                  <a href="#" className="font-semibold hover:text-slate-500">
                     Forgot password?
                   </a>
                 </div>
@@ -113,20 +101,14 @@ export default function Login() {
                 >
                   Sign in
                 </button>
-                <button
-                  onClick={() => signOutUser(navigate)}
-                  // type="submit"
-                  className="flex w-full justify-center rounded-md bg-red-600 mt-2 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign Out
-                </button>
-                <button
+
+                {/* <button
                   onClick={() => getOnlineUsers()}
                   // type="submit"
                   className="flex w-full justify-center rounded-md bg-green-600 mt-2 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   getOnlineUsers
-                </button>
+                </button> */}
               </div>
             </form>
 
