c80b36ad8909efba15933d4edd6ba466db379d41
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-10 21:18:17 +1000

    remove button

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -99,14 +99,6 @@ export default function Login() {
                 >
                   Sign in
                 </button>
-
-                {/* <button
-                  onClick={() => getOnlineUsers()}
-                  // type="submit"
-                  className="flex w-full justify-center rounded-md bg-green-600 mt-2 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  getOnlineUsers
-                </button> */}
               </div>
             </form>
 
