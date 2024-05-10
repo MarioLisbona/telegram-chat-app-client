7711ad8c47508a9dccf782dc7bf367b1674c5847
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-10 20:00:48 +1000

    add email type to input

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -37,7 +37,7 @@ export default function Register() {
                     onChange={(e) => console.log(e.target.value)}
                     id="email"
                     name="email"
-                    // type="email"
+                    type="email"
                     autoComplete="email"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -65,10 +65,7 @@ export default function Register() {
               </div>
 
               <div>
-                <button
-                  // type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
+                <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                   Create account
                 </button>
               </div>
