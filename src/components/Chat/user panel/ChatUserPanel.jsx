81259e6a6a6cff5b339d262219b99e59caed49af
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-10 14:23:43 +1000

    Remove newUserResponse socket response
    The application users array is not being sent from the server over the socket connection. It will now be returned from a function inside ActiveUser component

--- src/components/Chat/user panel/ChatUserPanel.jsx
+++ src/components/Chat/user panel/ChatUserPanel.jsx
@@ -4,17 +4,11 @@ import ActiveUsers from "./components/ActiveUsers";
 import { useState, useEffect } from "react";
 
 export default function ChatUserPanel({ socket }) {
-  const [users, setUsers] = useState([]);
-
-  // receive the array of active users
-  useEffect(() => {
-    socket.on("newUserResponse", (data) => setUsers(data));
-  }, [socket, users]);
   return (
     <div className="flex flex-col py-8 pl-6 pr-2 w-80 bg-white flex-shrink-0">
       <ChatName />
       {/* <UserProfile /> */}
-      <ActiveUsers users={users} socket={socket} />
+      <ActiveUsers socket={socket} />
     </div>
   );
 }
