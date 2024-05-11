54f5c5ecd44b8ee408c151da0cc1611c15a62b8c
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-11 22:39:19 +1000

    Create formatDateTime function

--- src/lib/generalUtils.js
+++ src/lib/generalUtils.js
@@ -1,13 +1,16 @@
-const now = new Date();
-export const formattedDateTime =
-  now.toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  }) +
-  " " +
-  now.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  });
+export const formatDateTime = (now) => {
+  const formattedDateTime =
+    now.toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    }) +
+    " " +
+    now.toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+
+  return formattedDateTime;
+};
