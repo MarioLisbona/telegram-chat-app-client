1dfd0b2387e967465a087138bdde65788c41d47f
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-12 22:33:53 +1000

    Create formatChatDateTime

--- src/lib/generalUtils.js
+++ src/lib/generalUtils.js
@@ -14,3 +14,37 @@ export const formatDateTime = (now) => {
 
   return formattedDateTime;
 };
+
+export const formatChatDateTime = (dateTimeString) => {
+  const dateTime = new Date(dateTimeString);
+  const now = new Date();
+
+  // Check if the date is today
+  const isToday = now.toDateString() === dateTime.toDateString();
+
+  let formattedDateTime;
+  if (isToday) {
+    formattedDateTime =
+      "Today " +
+      dateTime.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+      });
+  } else {
+    formattedDateTime =
+      dateTime.toLocaleDateString("en-US", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      }) +
+      " " +
+      dateTime.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+      });
+  }
+
+  return formattedDateTime;
+};
