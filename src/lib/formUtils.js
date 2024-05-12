5019988f7973a13d0a9a7b0755767ed3982cd64a
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-12 22:07:50 +1000

    Delete formUtils.js

--- src/lib/formUtils.js
+++
@@ -1,8 +0,0 @@
-export const handleSubmit = (socket, userName, navigate) => {
-  return (e) => {
-    e.preventDefault();
-    localStorage.setItem("userName", userName);
-    socket.emit("newUser", { userName, socketID: socket.id });
-    navigate("/chat");
-  };
-};
