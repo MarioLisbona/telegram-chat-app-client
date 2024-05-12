de1385df0cae7fa6f6416f69856d2d8890d9543d
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-13 09:24:42 +1000

    Remove use of lastMessageRef

--- src/components/Chat/body/ChatBody.jsx
+++ src/components/Chat/body/ChatBody.jsx
@@ -1,13 +1,15 @@
 import ChatFooter from "../footer/ChatFooter";
 import ChatMessageReceived from "./components/ChatMessageReceived";
 import ChatMessageSent from "./components/ChatMessageSent";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../../lib/firebase";
 import { getOnlineUsers } from "../../../lib/firebase";
-export default function ChatBody({ messages, socket, lastMessageRef }) {
+
+export default function ChatBody({ messages, socket }) {
   const [onlineUsers, setOnlineUsers] = useState([]);
   const [user] = useAuthState(auth);
+  const chatBodyRef = useRef(null);
 
   // This is working right now.
   // TODO: create function to return This user object
@@ -18,10 +20,20 @@ export default function ChatBody({ messages, socket, lastMessageRef }) {
   // UserObject data for this user from firestore
   const thisUserObject = onlineUsers[0];
 
+  // Scroll to the bottom of the chat window when messages change
+  useEffect(() => {
+    if (chatBodyRef.current) {
+      chatBodyRef.current.scrollTop = chatBodyRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
     <div className="flex flex-col flex-auto h-full p-6">
       <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
-        <div className="flex flex-col h-full overflow-x-auto mb-4">
+        <div
+          ref={chatBodyRef}
+          className="flex flex-col h-full overflow-x-auto mb-4"
+        >
           <div className="flex flex-col h-full">
             <div className="grid grid-cols-12 gap-y-2">
               {messages.map((message, idx) =>
@@ -33,8 +45,6 @@ export default function ChatBody({ messages, socket, lastMessageRef }) {
               )}
             </div>
           </div>
-          {/* used to scroll to bottom of element on new message received */}
-          <div ref={lastMessageRef} />
         </div>
         <ChatFooter socket={socket} />
       </div>
