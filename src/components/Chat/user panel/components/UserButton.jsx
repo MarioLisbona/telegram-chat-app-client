fa5fcdf7ab155a3a46ce1a1d9018aaf13c7835ff
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-14 11:24:48 +1000

    comment out old typing status

--- src/components/Chat/user panel/components/UserButton.jsx
+++ src/components/Chat/user panel/components/UserButton.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { returnBgColor } from "../../../../lib/colorsUtils";
 
 export default function UserButton({ onlineUser, socket }) {
+  // eslint-disable-next-line no-unused-vars
   const [showTypingStatus, setShowTypingStatus] = useState(false);
 
   // useEffect to set timeout for rendering component when user is typing
@@ -19,9 +20,10 @@ export default function UserButton({ onlineUser, socket }) {
     return () => {
       socket.off("typingResponse");
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socket]);
 
-  // variable to grab User intiial
+  // variable to grab User initial
   const userInitial = onlineUser.name.charAt(0).toUpperCase();
 
   const name = onlineUser.name;
@@ -32,19 +34,20 @@ export default function UserButton({ onlineUser, socket }) {
       <div
         className={`flex items-center justify-center h-8 w-8 rounded-full ${bgColorName}`}
       >
-        {showTypingStatus ? (
+        {/* {showTypingStatus ? (
           <span className="relative flex h-6 w-6">
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-400 opacity-75"></span>
           </span>
         ) : (
           userInitial
-        )}
+        )} */}
+        {userInitial}
       </div>
       <div className="flex flex-col justify-center items-start">
         <div className="ml-2 text-sm">
           <div className={"font-semibold"}>{name}</div>
         </div>
-        {showTypingStatus && <div className="text-xs ml-2">typing</div>}
+        {/* {showTypingStatus && <div className="text-xs ml-2">typing</div>} */}
       </div>
     </button>
   );
