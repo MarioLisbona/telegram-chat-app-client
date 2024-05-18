d4575531293fe4dd9025eed2313da2e8ed672810
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-18 12:10:38 +1000

    Remove / from socket connection URL

--- src/lib/socketUtils.js
+++ src/lib/socketUtils.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 export const initializeSocket = async () => {
   const url = import.meta.env.VITE_SERVER_URL || "http://localhost:4000";
   try {
-    const response = await fetch(`${url}/api`);
+    const response = await fetch(`${url}api`);
     const data = await response.json();
     const serverUrl = data.serverUrl;
     const socket = io(serverUrl, {
