3159a036811570d937cd1241e30d9f22c2bbf608
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-18 12:37:02 +1000

    conditional protocol assignment for socket

--- src/lib/socketUtils.js
+++ src/lib/socketUtils.js
@@ -4,10 +4,24 @@ import { io } from "socket.io-client";
 export const initializeSocket = async () => {
   const url = import.meta.env.VITE_SERVER_URL || "http://localhost:4000";
   try {
-    const response = await fetch(`${url}api`);
+    const response = await fetch(`${url}/api`);
     const data = await response.json();
     const serverUrl = data.serverUrl;
-    const socket = io(serverUrl, {
+
+    console.log("serverUrl--->", serverUrl);
+
+    // Check if the current protocol is HTTPS
+    const socketProtocol =
+      window.location.protocol === "https:" ? "https:" : "http:";
+
+    console.log("socketProtocol--->", socketProtocol);
+
+    // Construct the WebSocket URL
+    const socketUrl = serverUrl.replace(/^http/, socketProtocol.slice(0, -1));
+
+    console.log("socketUrl after replace--->", socketUrl);
+
+    const socket = io(socketUrl, {
       transports: ["websocket"],
     });
     return socket;
