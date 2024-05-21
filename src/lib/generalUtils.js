a9f707468c3597e6e34cf423da2943b743e9d8e2
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-21 12:25:59 +1000

    try-catch for formatChatDateTime

--- src/lib/generalUtils.js
+++ src/lib/generalUtils.js
@@ -22,37 +22,43 @@ export const formatDateTime = (now) => {
 };
 
 export const formatChatDateTime = (dateTimeString) => {
-  const dateTime = new Date(dateTimeString);
-  const now = new Date();
+  try {
+    const dateTime = new Date(dateTimeString);
+    const now = new Date();
 
-  // Check if the date is today
-  const isToday = now.toDateString() === dateTime.toDateString();
+    // Check if the date is today
+    const isToday = now.toDateString() === dateTime.toDateString();
 
-  let formattedDateTime;
-  if (isToday) {
-    formattedDateTime =
-      "Today " +
-      dateTime.toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-      });
-  } else {
-    formattedDateTime =
-      dateTime.toLocaleDateString("en-US", {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-      }) +
-      " " +
-      dateTime.toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-      });
+    let formattedDateTime;
+    if (isToday) {
+      formattedDateTime =
+        "Today " +
+        dateTime.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        });
+    } else {
+      formattedDateTime =
+        dateTime.toLocaleDateString("en-US", {
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        }) +
+        " " +
+        dateTime.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        });
+    }
+
+    return formattedDateTime;
+  } catch (error) {
+    console.error("Error in formatChatDateTime:", error);
+    // Handle the error appropriately, or rethrow it if needed
+    throw error;
   }
-
-  return formattedDateTime;
 };
 
 export const fetchChatTitle = async (setChatTitle) => {
