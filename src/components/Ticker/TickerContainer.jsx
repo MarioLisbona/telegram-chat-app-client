046770df83dafbd4cce9430b66b8f8b91dec34c0
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-22 15:52:29 +1000

    Create TickerContainer.jsx

---
+++ src/components/Ticker/TickerContainer.jsx
@@ -0,0 +1,9 @@
+import TickerBar from "./TickerBar";
+
+export default function TickerContainer() {
+  return (
+    <div className="m-4 p-2 bg-slate-300 ">
+      <TickerBar />
+    </div>
+  );
+}
