43b84b2eb20cbb7e7b9e3ea769a999131e44fbe1
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-23 10:56:40 +1000

    Update ChatBody.jsx

--- src/components/Chat/body/ChatBody.jsx
+++ src/components/Chat/body/ChatBody.jsx
@@ -28,7 +28,6 @@ export default function ChatBody({
     <div className="flex flex-col flex-auto h-full p-6 ">
       <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4 ">
         <TickerContainer />
-
         <div
           ref={chatBodyRef}
           className="flex flex-col h-full overflow-x-auto mb-4"
