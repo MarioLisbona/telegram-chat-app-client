c2ed849b377a9113e7aace8741e840024d994666
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-24 09:34:01 +1000

    update marquee container height and bg

--- src/components/Ticker/MarqueeContainer.jsx
+++ src/components/Ticker/MarqueeContainer.jsx
@@ -2,14 +2,17 @@ import Marquee from "react-fast-marquee";
 
 export default function MarqueeContainer({ children, speed = 70 }) {
   return (
-    <Marquee
-      pauseOnHover
-      speed={speed}
-      gradient
-      gradientColor={"#F3F4F6"}
-      gradientWidth={50}
-    >
-      {children}
-    </Marquee>
+    <div className="w-full overflow-hidden bg-gray-100 ">
+      <Marquee
+        play={false}
+        pauseOnHover
+        speed={speed}
+        gradient
+        gradientColor={"#F3F4F6"}
+        gradientWidth={50}
+      >
+        {children}
+      </Marquee>
+    </div>
   );
 }
