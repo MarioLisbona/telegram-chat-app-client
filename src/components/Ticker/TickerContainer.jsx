8c71fce624d1b818ad6891c4cb234b54a7b00824
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-23 11:25:19 +1000

    Remove fetchTokenData and old mapping

--- src/components/Ticker/TickerContainer.jsx
+++ src/components/Ticker/TickerContainer.jsx
@@ -3,28 +3,7 @@ import MarqueeContainer from "./MarqueeContainer";
 import { fetchTickerData } from "../../lib/chatUtils";
 
 export default function TickerContainer() {
-  const [first20Tokens, setFirst20Tokens] = useState([]);
-  const [last20Tokens, setLast20Tokens] = useState([]);
   const [tickerData, setTickerData] = useState([]);
-
-  useEffect(() => {
-    const fetchTokenData = async () => {
-      try {
-        const response = await fetch("http://localhost:4000/api/tokens");
-        if (!response.ok) {
-          throw new Error("Failed to fetch token data");
-        }
-        const data = await response.json();
-        console.log("data inside useeffect", data);
-        setFirst20Tokens(data.firstTwentyPrices);
-        setLast20Tokens(data.lastTwentyPrices);
-      } catch (error) {
-        console.error("Error fetching messages:", error);
-      }
-    };
-
-    fetchTokenData();
-  }, []);
 
   useEffect(() => {
     fetchTickerData(setTickerData);
@@ -35,7 +14,10 @@ export default function TickerContainer() {
     <div className="my-4 rounded-lg bg-gray-200">
       <MarqueeContainer>
         {tickerData.map((coin, idx) => (
-          <div key={idx}>
+          <div
+            key={idx}
+            onClick={() => console.log("Clicking this token data", coin)}
+          >
             <img
               src={coin.image}
               alt={coin.symbol}
@@ -45,22 +27,6 @@ export default function TickerContainer() {
           </div>
         ))}
       </MarqueeContainer>
-      {/* <MarqueeContainer>
-        {first20Tokens.map((token, idx) => (
-          <div
-            onClick={() => console.log("Clicking this token data", token)}
-            key={idx}
-          >{` --- ${token.symbol}: ${token.price} ${" "}`}</div>
-        ))}
-      </MarqueeContainer>
-      <MarqueeContainer speed={130}>
-        {last20Tokens.map((token, idx) => (
-          <div
-            onClick={() => console.log("Clicking this token data", token)}
-            key={idx}
-          >{` --- ${token.symbol}: ${token.price} ${" "}`}</div>
-        ))}
-      </MarqueeContainer> */}
     </div>
   );
 }
