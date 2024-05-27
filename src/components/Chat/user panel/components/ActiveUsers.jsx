ae84f8b4109ea733628f52008805da9146917249
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-27 13:30:04 +1000

    Active users panel styling

--- src/components/Chat/user panel/components/ActiveUsers.jsx
+++ src/components/Chat/user panel/components/ActiveUsers.jsx
@@ -21,19 +21,31 @@ export default function ActiveUser({ socket }) {
   }, [user]); // Update when user changes
 
   return (
-    <div className="flex flex-col mt-8">
-      <div className="flex flex-row items-center justify-start text-xs mx-1">
-        <span className="flex items-center justify-center bg-gray-300 h-6 w-6 rounded-full ">
+    // <div className="flex flex-col mt-8">
+    //   <div className="flex flex-row items-center justify-start text-xs mx-1">
+    //     <span className="flex items-center justify-center bg-gray-300 h-6 w-6 rounded-full ">
+    //       {onlineUsers.length}
+    //     </span>
+    //     <span className="font-bold mx-2">Active Users</span>
+    //   </div>
+    <div className="flex flex-col space-y-1 mt-4 overflow-y-auto">
+      <button className="flex flex-row items-center bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 shadow rounded-2xl me-2">
+        <div
+          className={`flex items-center justify-center h-8 w-8 rounded-full bg-indigo-500`}
+        >
           {onlineUsers.length}
-        </span>
-        <span className="font-bold mx-2">Active Users</span>
-      </div>
-      <div className="flex flex-col space-y-1 mt-4 -mx-2 overflow-y-auto">
-        {onlineUsers &&
-          onlineUsers.map((onlineUser, idx) => (
-            <UserButton onlineUser={onlineUser} key={idx} socket={socket} />
-          ))}
-      </div>
+        </div>
+        <div className="flex flex-col justify-center items-start">
+          <div className="ml-2 text-sm">
+            <div className="font-semibold">{"Active users"}</div>
+          </div>
+        </div>
+      </button>
+      {onlineUsers &&
+        onlineUsers.map((onlineUser, idx) => (
+          <UserButton onlineUser={onlineUser} key={idx} socket={socket} />
+        ))}
     </div>
+    // </div>
   );
 }
