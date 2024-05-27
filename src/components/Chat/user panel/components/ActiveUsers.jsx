5cc3ee8e98771b3903ed580975a2db6888c17029
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-27 13:43:59 +1000

    slide active users up for hide and down for show

--- src/components/Chat/user panel/components/ActiveUsers.jsx
+++ src/components/Chat/user panel/components/ActiveUsers.jsx
@@ -1,7 +1,7 @@
 import UserButton from "./UserButton";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../../../lib/firebase";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getOnlineUsers } from "../../../../lib/firebase";
 
 export default function ActiveUser({ socket }) {
@@ -10,6 +10,8 @@ export default function ActiveUser({ socket }) {
   // eslint-disable-next-line no-unused-vars
   const [userId, setUserId] = useState("");
   const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const containerRef = useRef(null);
 
   const toggleCollapse = () => {
     setIsCollapsed(!isCollapsed);
@@ -27,10 +29,11 @@ export default function ActiveUser({ socket }) {
 
   return (
     <div className="flex flex-col space-y-1 mt-4 overflow-y-auto">
-      <button className="flex flex-row items-center justify-between bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 shadow rounded-2xl me-2">
-        <div
-          className={`flex items-center justify-center h-8 w-8 rounded-full bg-indigo-500`}
-        >
+      <button
+        className="flex flex-row items-center justify-between bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 shadow rounded-2xl me-2"
+        onClick={toggleCollapse}
+      >
+        <div className="flex items-center justify-center h-8 w-8 rounded-full bg-indigo-500">
           {onlineUsers.length}
         </div>
         <div className="flex flex-col justify-center items-start">
@@ -48,7 +51,6 @@ export default function ActiveUser({ socket }) {
             strokeWidth={1.5}
             stroke="currentColor"
             className="size-6"
-            onClick={toggleCollapse}
           >
             <path
               strokeLinecap="round"
@@ -64,7 +66,6 @@ export default function ActiveUser({ socket }) {
             strokeWidth={1.5}
             stroke="currentColor"
             className="size-6"
-            onClick={toggleCollapse}
           >
             <path
               strokeLinecap="round"
@@ -74,12 +75,17 @@ export default function ActiveUser({ socket }) {
           </svg>
         )}
       </button>
-      {onlineUsers && !isCollapsed
-        ? onlineUsers.map((onlineUser, idx) => (
-            <UserButton onlineUser={onlineUser} key={idx} socket={socket} />
-          ))
-        : ""}
+      <div
+        ref={containerRef}
+        className={`transition-max-height duration-300 ease-in-out overflow-hidden ${
+          isCollapsed ? "max-h-0" : "max-h-[500px]"
+        }`}
+        style={{ transitionProperty: "max-height" }}
+      >
+        {onlineUsers.map((onlineUser, idx) => (
+          <UserButton onlineUser={onlineUser} key={idx} socket={socket} />
+        ))}
+      </div>
     </div>
-    // </div>
   );
 }
