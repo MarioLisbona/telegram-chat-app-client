aafd28fe481784c055744e123f12ebc49e2212d0
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-29 13:31:33 +1000

    Update styling

--- src/components/Chat/body/components/UserJoinLeave.jsx
+++ src/components/Chat/body/components/UserJoinLeave.jsx
@@ -6,27 +6,20 @@ export default function UserJoinLeave({ message }) {
   const timeSent = formatChatCreatedAt(message.createdAt);
   return (
     <div className="col-start-6 col-end-13 p-3 rounded-lg">
-      <div className="flex flex-col items-end">
-        <div className="text-xs mb-1">{userName}</div>
+      <div className="flex flex-col items-start">
+        {/* <div className="text-xs mb-1">{userName}</div> */}
         <div className="flex items-start justify-end">
-          <div className="flex row-reverse">
+          <div className="flex row-reverse justify-center items-center">
             <div
-              className={`relative mr-3 text-sm text-black font-medium py-2 px-4 shadow rounded-xl rounded-tr-none max-w-1/2 flex-grow whitespace-pre-wrap ${
-                left ? " bg-rose-400" : " bg-emerald-400"
+              className={`relative text-xs font-medium bg-white px-4 py-1 shadow rounded-xl flex-grow border-solid border-2 ${
+                left ? " border-rose-400" : " border-emerald-400"
               }`}
             >
-              <div>{text}</div>
-            </div>
-            <div
-              className={`flex-shrink-0 top-0 right-0 flex items-center justify-center h-10 w-10 rounded-full ${
-                left ? " bg-rose-400" : " bg-emerald-400"
-              }`}
-            >
-              {"T"}
+              <div>{`${timeSent} - ${text}`}</div>
             </div>
           </div>
         </div>
-        <div className="text-xs mt-1">{timeSent}</div>
+        {/* <div className="text-xs mt-1">{timeSent}</div> */}
       </div>
     </div>
   );
