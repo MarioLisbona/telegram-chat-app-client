b891cf4cbab63a6aeb31bbaf6a2a09631efcef0a
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-29 21:23:18 +1000

    Install aspect-ratio library and update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 const forms = require("@tailwindcss/forms");
+const aspectRatio = require("@tailwindcss/aspect-ratio");
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [forms],
+  plugins: [forms, aspectRatio],
 };
