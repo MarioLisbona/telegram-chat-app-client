11d08de9567205e6f4f50b6a4a0000b47ac9ff0d
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-29 22:28:08 +1000

    Move files array to DataGrid

--- src/components/Chat/body/ChatBody.jsx
+++ src/components/Chat/body/ChatBody.jsx
@@ -25,42 +25,7 @@ export default function ChatBody({
     }
   }, [messages]);
 
-  const files = [
-    {
-      title: "IMG_4985.HEIC",
-      size: "3.9 MB",
-      source:
-        "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-    },
-    {
-      title: "IMG_4985.HEIC",
-      size: "3.9 MB",
-      source:
-        "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-    },
-    {
-      title: "IMG_4985.HEIC",
-      size: "3.9 MB",
-      source:
-        "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-    },
-    {
-      title: "IMG_4985.HEIC",
-      size: "3.9 MB",
-      source:
-        "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-    },
-    {
-      title: "IMG_4985.HEIC",
-      size: "3.9 MB",
-      source:
-        "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-    },
-    // More files...
-  ];
-
-  // eslint-disable-next-line no-unused-vars
-  const nofiles = [];
+  const tokenQuery = true;
 
   return (
     <div className="flex flex-col flex-auto h-full p-6 ">
@@ -89,7 +54,7 @@ export default function ChatBody({
                 </div>
               )}
             </div>
-            <DataGrid files={files} />
+            {tokenQuery && <DataGrid />}
           </div>
         </div>
         <ChatFooter socket={socket} />
