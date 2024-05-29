45b627b05718b7a77c727d930947164522a4ee51
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-05-29 22:28:20 +1000

    Update DataGrid.jsx

--- src/components/Chat/body/components/DataGrid.jsx
+++ src/components/Chat/body/components/DataGrid.jsx
@@ -1,12 +1,46 @@
 import { useState } from "react";
 import ChartData from "./ChartData";
 
-export default function DataGrid({ files }) {
+export default function DataGrid() {
   const [showDataGrid, setShowDataGrid] = useState(true);
 
   const toggleDataGrid = () => {
     setShowDataGrid(!showDataGrid);
   };
+
+  const files = [
+    {
+      title: "IMG_4985.HEIC",
+      size: "3.9 MB",
+      source:
+        "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+    },
+    {
+      title: "IMG_4985.HEIC",
+      size: "3.9 MB",
+      source:
+        "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+    },
+    {
+      title: "IMG_4985.HEIC",
+      size: "3.9 MB",
+      source:
+        "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+    },
+    {
+      title: "IMG_4985.HEIC",
+      size: "3.9 MB",
+      source:
+        "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+    },
+    {
+      title: "IMG_4985.HEIC",
+      size: "3.9 MB",
+      source:
+        "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+    },
+    // More files...
+  ];
 
   return (
     <div className="relative">
