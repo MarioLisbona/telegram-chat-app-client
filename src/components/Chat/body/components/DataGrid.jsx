039356c33fa9b8a577d937035540a831fd44fc32
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-06-02 17:43:54 +1000

    use token prop inside DataGrid

--- src/components/Chat/body/components/DataGrid.jsx
+++ src/components/Chat/body/components/DataGrid.jsx
@@ -1,55 +1,33 @@
 import ChartData from "./ChartData";
 
-export default function DataGrid() {
-  const data = [
-    {
-      title: "IMG_4985.HEIC",
-      size: "3.9 MB",
-    },
-    {
-      title: "IMG_4985.HEIC",
-      size: "3.9 MB",
-    },
-    {
-      title: "IMG_4985.HEIC",
-      size: "3.9 MB",
-    },
-    {
-      title: "IMG_4985.HEIC",
-      size: "3.9 MB",
-    },
-    {
-      title: "IMG_4985.HEIC",
-      size: "3.9 MB",
-    },
-    // More files...
-  ];
-
+export default function DataGrid({ token }) {
   return (
     <div className="relative">
       <ul
         role="list"
         className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-5 xl:gap-x-8"
       >
-        {data
-          ? data.map((dataPoint, index) => (
-              <li key={index} className="relative">
+        {token
+          ? token.historyData.map((dataPoint, index) => (
+              <li
+                key={index}
+                className="relative"
+                onClick={() => console.log("dataPoint", dataPoint.dataType)}
+              >
                 <div className="group aspect-w-10 aspect-h-7 block w-full overflow-hidden rounded-lg bg-gray-100 border border-transparent hover:border-violet-600 hover:bg-gray-200 transition duration-300 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-                  <ChartData />
+                  <ChartData dataPoint={dataPoint} />
                   <button
                     type="button"
                     className="absolute inset-0 focus:outline-none"
                   >
-                    <span className="sr-only">
-                      View details for {dataPoint.title}
-                    </span>
+                    <span className="sr-only">View details for {token.id}</span>
                   </button>
                 </div>
                 <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
-                  {dataPoint.title}
+                  {token.id}
                 </p>
                 <p className="pointer-events-none block text-sm font-medium text-gray-500">
-                  {dataPoint.size}
+                  {dataPoint.dataType}
                 </p>
               </li>
             ))
